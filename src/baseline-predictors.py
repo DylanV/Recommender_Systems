8303ad26f00f3a46a22ad4fc9e8d8216eaccc922
DylanV <dverrezen@gmail.com>
2016-03-11 09:10:13 +0200

    Calculate user_means method added to baseline

--- src/baseline-predictors.py
+++ src/baseline-predictors.py
@@ -6,3 +6,16 @@ class BaselinePredicor(object):
 
     def __init__(self, ratings):
         self.ratings = ratings
+        self.ratings_index = ratings.index
+        self.ratings_col = ratings.columns
+
+    def calculate_user_means(self):
+        """
+        Calculate the user means of the ratings matrix
+        :return: A DataFrame indexed on user_id with one column mean containinng the user means
+        """
+
+        ratings = self.ratings.replace(0, np.nan)
+        means = pd.DataFrame(ratings.mean(axis=1), index=self.ratings_index, columns=['mean']).fillna(value=0)
+        return means
+
