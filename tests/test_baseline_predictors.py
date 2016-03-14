6ad7a2f4135df807e797279e1ccb3c0738514632
DylanV <dverrezen@gmail.com>
2016-03-14 17:12:04 +0200

    Test for user std devs in baseline predictors

--- tests/test_baseline_predictors.py
+++ tests/test_baseline_predictors.py
@@ -5,22 +5,39 @@ from baseline_predictors import BaselinePredictor
 
 class TestBaselinePredictors(unittest.TestCase):
 
-    def setUp(self):
-        self.ratings_values = np.asarray([[2, 0, 4, 3, 0],
+    def test_init(self):
+        ratings_values = np.asarray([[2, 0, 4, 3, 0],
                                         [0, 3, 0, 3, 0],
                                         [4, 0, 0, 0, 5],
                                         [0, 3, 1, 0, 0],
                                         [5, 0, 4, 3, 1]])
-        self.ratings = pd.DataFrame(self.ratings_values)
-
-    def test_init(self):
-        bp = BaselinePredictor(self.ratings)
+        ratings = pd.DataFrame(ratings_values)
+        bp = BaselinePredictor(ratings)
         self.assertEqual(bp.ratings.shape, (5,5))
 
     def test_calculate_user_means(self):
-        bp = BaselinePredictor(self.ratings)
+        ratings_values = np.asarray([[2, 0, 4, 3, 0],
+                                        [0, 3, 0, 3, 0],
+                                        [4, 0, 0, 0, 5],
+                                        [0, 3, 1, 0, 0],
+                                        [5, 0, 4, 3, 1]])
+        ratings = pd.DataFrame(ratings_values)
+        bp = BaselinePredictor(ratings)
         true_user_means = np.asarray([3., 3., 4.5, 2., 3.25])
         means = bp.calculate_user_means(self.ratings)
         self.assertTrue(np.all((true_user_means - means.values[:, 0]) == 0))
         self.assertEqual(means.shape, (5, 1))
 
+    def test_calculate_user_std_devs(self):
+        ratings_values = np.asarray([[2, 0, 4, 3, 0],
+                                        [0, 3, 0, 3, 0],
+                                        [4, 0, 0, 0, 5],
+                                        [0, 3, 1, 0, 0],
+                                        [5, 0, 4, 3, 1]])
+        ratings = pd.DataFrame(ratings_values)
+        bp = BaselinePredictor(ratings)
+
+        true_user_std_devs = [np.sqrt(2./3.), 0.,0.5, 1.0, np.sqrt(2.1875)]
+        user_std_devs = bp.calculate_user_std_devs(ratings)
+        self.assertTrue(np.all((true_user_std_devs - user_std_devs.values[:, 0]) == 0))
+        self.assertEqual(user_std_devs.shape, (5, 1))
