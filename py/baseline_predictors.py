4a903d3209bf67c39b8b411768685d76deb64eb9
DylanV <dverrezen@gmail.com>
2016-05-19 11:45:03 +0200

    Moved python source from src to py

---
+++ py/baseline_predictors.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+
+from extras import clamp
+
+class BaselinePredictor(object):
+
+    def __init__(self, ratings):
+        """
+        :param ratings: The ratings DataFrame
+        :type ratings: DataFrame
+        """
+        self.ratings = ratings
+
+    @staticmethod
+    def calculate_user_means(ratings):
+        """
+        Calculate the user means of the ratings matrix
+        :param ratings: The ratings DataFrame
+        :type ratings: DataFrame
+        :return: A DataFrame indexed on user_id with one column mean with the user means
+        """
+        ratings = ratings.replace(0, np.nan)
+        means = pd.DataFrame(ratings.mean(axis=1), index=ratings.index, columns=['mean']).fillna(value=0)
+        return means
+
+    @staticmethod
+    def calculate_user_std_devs(ratings):
+        """
+        Calculate the user standard deviations in the user x item DataFrame
+        :param ratings: the ratings DataFrame
+        :type ratings: DataFrame
+        :return: A DataFrame indexed on user_id with one column std with the user standard deviations
+        """
+        ratings = ratings.replace(0, np.nan)
+        std_devs = pd.DataFrame(ratings.std(axis=1, ddof=0), index=ratings.index, columns=['std']).fillna(value=0)
+        return std_devs
+
+    @staticmethod
+    def calculate_item_means(ratings):
+        """
+        Calculate the item means
+        :param ratings: ratings DataFrame
+        :type ratings: DataFrame
+        :return: The item means
+        """
+        ratings = ratings.replace(0, np.nan)
+        means = pd.DataFrame(ratings.mean(axis=0), index=ratings.columns, columns=['mean']).fillna(value=0)
+        return means
+
+    def predict_user_based(self):
+        """
+        Calculate a baseline prediction based on user means
+        :return: A prediction DataFrame same shape as ratings
+        """
+        user_means = self.calculate_user_means(self.ratings)
+        predicted = pd.DataFrame(np.ones(self.ratings.shape) * user_means.values,
+                                 index=self.ratings.index, columns=self.ratings.columns)
+        predicted = predicted.applymap(clamp)
+        return predicted
+
+    def predict_item_based(self):
+        """
+        Calculate a baseline prediction based on item means
+        :return: A DataFrame in the same shape as ratings with predictions as values
+        """
+        movie_means = self.calculate_item_means(self.ratings)
+        predicted = pd.DataFrame((np.ones(self.ratings.shape).transpose() * movie_means.values).transpose(),
+                                 index=self.ratings.index, columns=self.ratings.columns)
+        predicted = predicted.applymap(clamp)
+        return predicted
+
+    def predict_item_user_based(self):
+        """
+        Calculate a baseline prediction based on item means and user average offsets
+        :return: The prediction DataFrame
+        """
+        movie_means = self.calculate_item_means(self.ratings)
+        user_std_devs = self.calculate_user_std_devs(self.ratings)
+        predicted_values = (np.ones(self.ratings.shape).transpose() * movie_means.values).transpose()
+        predicted_values = predicted_values + user_std_devs.values
+        predicted = pd.DataFrame(predicted_values, index=self.ratings.index, columns=self.ratings.columns)
+        predicted = predicted.applymap(clamp)
+        return predicted
---
+++ py/collaborative_filtering.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+
+from extras import clamp
+
+class CollaborativeFiltering(object):
+
+    def __init__(self, ratings):
+        """
+        :param ratings:  The user x item ratings DataFrame. Ratings on 1 to 5 scale. 0 represents unknown.
+        :type ratings: DataFrame
+        """
+        self.ratings = ratings
+        self.user_means = self.calculate_user_means(self.ratings)
+        self.user_std_devs = self.calculate_user_std_devs(self.ratings)
+
+    @staticmethod
+    def calculate_user_means(ratings):
+        """
+        Calculate the user means of the ratings matrix
+        :param ratings: The ratings DataFrame
+        :type ratings: DataFrame
+        :return: A DataFrame indexed on user_id with one column mean with the user means
+        """
+        ratings = ratings.replace(0, np.nan)
+        means = pd.DataFrame(ratings.mean(axis=1), index=ratings.index, columns=['mean']).fillna(value=0)
+        return means
+
+    @staticmethod
+    def calculate_user_std_devs(ratings):
+        """
+        Calculate the user standard deviations in the user x item DataFrame
+        :param ratings: the ratings DataFrame
+        :type ratings: DataFrame
+        :return: A DataFrame indexed on user_id with one column std with the user standard deviations
+        """
+        ratings = ratings.replace(0, np.nan)
+        std_devs = pd.DataFrame(ratings.std(axis=1, ddof=0), index=ratings.index, columns=['std']).fillna(value=0)
+        return std_devs
+
+    @staticmethod
+    def get_user_similarity(ratings, method='cosine'):
+        """
+        Calculate the user similarity matrix
+        :param ratings: The ratings DataFrame
+        :type ratings: DataFrame
+        :param method: <'cosine', 'pearson'> the similarity measure to be used
+        :return:
+        """
+        if method == 'cosine':
+            user_similarity = ratings.dot(ratings.transpose())
+            normalisation_terms = pd.DataFrame(np.diagonal(user_similarity.values), index=ratings.index).apply(np.sqrt)
+            normalisation_terms = normalisation_terms.dot(normalisation_terms.transpose())
+            user_similarity = user_similarity.div(normalisation_terms)
+            return user_similarity
+        elif method == 'pearson':
+            return ratings.transpose().corr(method='pearson')
+        else:
+            raise KeyError(method+' is not an implemented method.')
+
+    @staticmethod
+    def adjust_user_similarity_knn(user_similarity, k):
+        """
+        Adjust the user similarity matrix for use with k-nearest neighbours.
+        For each user the top k neighbours are kept and for the rest the similarity is set to 0.
+        Thus in the prediction their ratings will be ignored in the calculation.
+        :param user_similarity: The user similarity DataFrame
+        :param k: the number of neighbours for k-nearest neighbour
+        :return: The adjusted similarity DataFrame
+        """
+        adjusted_similarity = pd.DataFrame(np.zeros(user_similarity.shape),
+                                           index=user_similarity.index, columns=user_similarity.columns)
+
+        for user in user_similarity.iterrows():
+            top_k_indexes = user[1].sort_values(ascending=False).iloc[0:k+1].index.values
+            adjusted_similarity.loc[user[0], top_k_indexes] = user_similarity.loc[user[0], top_k_indexes]
+
+        return adjusted_similarity
+
+    def adjust_ratings(self, ratings, type='mean'):
+        """
+        Adjust the ratings matrix from a 1-5 ratings scale to a -1 to 1 scale indicating user preference
+        :param ratings: The ratings DataFrame
+        :param type: means or full
+        :return: The adjusted ratings DataFrame
+        """
+        user_means = self.user_means
+        adjusted_ratings = ratings.replace(0, np.nan)
+        adjusted_ratings_values = adjusted_ratings.values - user_means.values
+        if type == 'full':
+            adjusted_ratings_values = adjusted_ratings_values / self.user_std_devs.values
+        adjusted_ratings = pd.DataFrame(adjusted_ratings_values, index=ratings.index, columns=ratings.columns).fillna(value=0)
+
+        return adjusted_ratings
+
+    def adjust_predictions(self, predicted,  type='mean'):
+        """
+        Adjust the predictions back to a 1-5 ratings scale
+        :param predicted: The predicted DataFrame
+        :param user_means: The user means
+        :param user_std_devs: The user standard deviations. Optional
+        :return: The adjusted predictions
+        """
+        if type == 'full':
+            adjusted_predictions_values = predicted.values * self.user_std_devs.values
+        else:
+            adjusted_predictions_values = predicted.values
+        adjusted_predicted_values = adjusted_predictions_values + self.user_means.values
+        adjusted_predictions = pd.DataFrame(adjusted_predicted_values,
+                                            index=predicted.index, columns=predicted.columns)
+        adjusted_predictions = adjusted_predictions.fillna(value=0).applymap(clamp)
+        return adjusted_predictions
+
+    def predict_user_user(self, adjust='full', similarity='cosine', k=-1):
+        """
+        Do user user prediction.
+        Options to use mean or mean and std-dev (full) adjusted ratings (default full),
+        Cosine or pearson correlation for user similarity (default cosine),
+        and the number of neighbours to consider (default all).
+        :param k: The number of neighbours for k-nearest neighbours
+        :param adjust: <'mean','full'> adjust the ratings for user means or means and standard deviations respectively
+        :param similarity: <'cosine', 'pearson'> The similarity measure for the user similarity
+        :return: The prediction DataFrame
+        """
+        ratings = self.ratings
+        if adjust == 'mean':
+            ratings = self.adjust_ratings(ratings, self.user_means)
+        elif adjust == 'full':
+            ratings = self.adjust_ratings(ratings, self.user_means, self.user_std_devs)
+
+        user_similarity = self.get_user_similarity(ratings, method=similarity)
+        if k != -1:
+            user_similarity = self.adjust_user_similarity_knn(user_similarity, k)
+
+        predictions = user_similarity.dot(ratings)
+        denom = user_similarity.abs().sum().transpose()
+        predictions = predictions.div(denom, axis='index')
+
+        if adjust == 'mean':
+            predictions = self.adjust_predictions(predictions, self.user_means)
+        elif adjust == 'full':
+            predictions = self.adjust_predictions(self.user_means, self.user_std_devs)
+
+        predictions = pd.DataFrame(predictions.values, index=self.ratings.index, columns=self.ratings.columns)
+        return predictions
